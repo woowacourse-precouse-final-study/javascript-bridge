d39ac6ddbe3bf8d0143e577242871aa6050ffb77
rachel490 <ccori12@naver.com>
2022-11-21 01:30:54 +0900

    feat: answerBridge 생성 controller 구현, readBridgeSize 로직 추가

--- src/Controller.js
+++ src/Controller.js
@@ -1,11 +1,13 @@
 const BridgeGame = require('./BridgeGame');
+const BridgeMaker = require('./BridgeMaker');
+const BridgeRandomNumberGenerator = require('./BridgeRandomNumberGenerator');
 const { GAME_GUIDE_MESSAGES } = require('./constants');
 const InputView = require('./InputView');
 const OutputView = require('./OutputView');
 
 class Controller {
 	constructor() {
-		this.brigeGameModel = new BridgeGame();
+		this.bridgeGameModel = new BridgeGame();
 		this.bridgeGameView = {
 			OutputView,
 			InputView,
@@ -14,6 +16,14 @@ class Controller {
 
 	start() {
 		this.bridgeGameView.OutputView.printMessage(GAME_GUIDE_MESSAGES.START);
+		this.createBridge();
+	}
+
+	createBridge() {
+		this.bridgeGameView.InputView.readBridgeSize(sizeInput => {
+			const answerBridge = BridgeMaker.makeBridge(Number(sizeInput), BridgeRandomNumberGenerator.generate);
+			this.bridgeGameModel.setState({ answerBridge });
+		});
 	}
 }
 
--- src/InputView.js
+++ src/InputView.js
@@ -1,21 +1,28 @@
+const { Console } = require('@woowacourse/mission-utils');
+const { GAME_GUIDE_MESSAGES } = require('./constants');
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 const InputView = {
-  /**
-   * 다리의 길이를 입력받는다.
-   */
-  readBridgeSize() {},
+	/**
+	 * 다리의 길이를 입력받는다.
+	 */
+	readBridgeSize(createBridgeController) {
+		Console.readLine(GAME_GUIDE_MESSAGES.LENGHT_INPUT, sizeInput => {
+			Console.print('\n');
+			createBridgeController(sizeInput);
+		});
+	},
+	/**
+	 * 사용자가 이동할 칸을 입력받는다.
+	 */
+	readMoving() {},
 
-  /**
-   * 사용자가 이동할 칸을 입력받는다.
-   */
-  readMoving() {},
-
-  /**
-   * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
-   */
-  readGameCommand() {},
+	/**
+	 * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
+	 */
+	readGameCommand() {},
 };
 
 module.exports = InputView;
