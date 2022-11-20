8b8d43be4fc5835079d9c3641badc69f6268205f
rachel490 <ccori12@naver.com>
2022-11-21 01:22:41 +0900

    feat: message constants에 개행 문자 추가

--- src/constants.js
+++ src/constants.js
@@ -1,10 +1,10 @@
 const GAME_GUIDE_MESSAGES = {
-	START: '다리 건너기 게임을 시작합니다.',
-	LENGHT_INPUT: '다리의 길이를 입력해주세요.',
-	MOVE_INPUT: '이동할 칸을 선택해주세요. (위: U, 아래: D)',
-	COMMAND_INPUT: '게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)',
+	START: '다리 건너기 게임을 시작합니다.\n',
+	LENGHT_INPUT: '다리의 길이를 입력해주세요.\n',
+	MOVE_INPUT: '이동할 칸을 선택해주세요. (위: U, 아래: D)\n',
+	COMMAND_INPUT: '게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)\n',
 	FINAL_RESULT_OUTPUT: {
-		MESSAGE: '최종 게임 결과',
+		MESSAGE: '최종 게임 결과\n',
 		RESULT: '게임 성공 여부: ',
 		TRIALS: '총 시도한 횟수: ',
 	},
