6d161a57c932f0138804b40f5f48d50feb93c843
rachel490 <ccori12@naver.com>
2022-11-20 22:26:53 +0900

    feat: 게임 진행에 필요한 메세지 constants 파일에 추가

---
+++ src/constants.js
@@ -0,0 +1,15 @@
+const GAME_GUIDE_MESSAGES = {
+	START: '다리 건너기 게임을 시작합니다.',
+	LENGHT_INPUT: '다리의 길이를 입력해주세요.',
+	MOVE_INPUT: '이동할 칸을 선택해주세요. (위: U, 아래: D)',
+	COMMAND_INPUT: '게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)',
+	FINAL_RESULT_OUTPUT: {
+		MESSAGE: '최종 게임 결과',
+		RESULT: '게임 성공 여부: ',
+		TRIALS: '총 시도한 횟수: ',
+	},
+};
+
+module.exports = {
+	GAME_GUIDE_MESSAGES,
+};
