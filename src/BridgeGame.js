fe7571b42314784ded34a1bddba0b18943be6683
rachel490 <ccori12@naver.com>
2022-11-21 02:48:52 +0900

    feat: 이동한 칸 표시하는 로직 추가

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -26,7 +26,35 @@ class BridgeGame {
 	 * 이동을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
 	 */
 	move(directionInput) {
-		console.log(directionInput);
+		const { answerBridge, currentLocation } = this.state;
+		const isAnswer = directionInput === answerBridge[currentLocation];
+		this.generateCurrentUserBridge(isAnswer, directionInput);
+
+		console.log(this.state);
+	}
+
+	generateCurrentUserBridge(isAnswer, directionInput) {
+		if (isAnswer) {
+			if (directionInput === 'U') {
+				const newUp = [...this.state.currentUserBridge.up, 'O'];
+				const newDown = [...this.state.currentUserBridge.down, ' '];
+				this.setState({ currentUserBridge: { up: newUp, down: newDown } });
+			} else if (directionInput === 'D') {
+				const newUp = [...this.state.currentUserBridge.up, ' '];
+				const newDown = [...this.state.currentUserBridge.down, 'O'];
+				this.setState({ currentUserBridge: { up: newUp, down: newDown } });
+			}
+		} else {
+			if (directionInput === 'U') {
+				const newUp = [...this.state.currentUserBridge.up, 'X'];
+				const newDown = [...this.state.currentUserBridge.down, ' '];
+				this.setState({ currentUserBridge: { up: newUp, down: newDown } });
+			} else if (directionInput === 'D') {
+				const newUp = [...this.state.currentUserBridge.up, ' '];
+				const newDown = [...this.state.currentUserBridge.down, 'X'];
+				this.setState({ currentUserBridge: { up: newUp, down: newDown } });
+			}
+		}
 	}
 
 	/**
