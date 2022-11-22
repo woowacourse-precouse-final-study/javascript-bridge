468a5e44248889cbfc24d547390ec9da796c0913
rachel490 <ccori12@naver.com>
2022-11-22 22:32:30 +0900

    style: 오프닝 메세지 개행 제거

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,5 @@
 const GAME_GUIDE_MESSAGES = {
-	START: '다리 건너기 게임을 시작합니다.\n',
+	START: '다리 건너기 게임을 시작합니다.',
 	LENGTH_INPUT: '\n다리의 길이를 입력해주세요.\n',
 	MOVE_INPUT: '\n이동할 칸을 선택해주세요. (위: U, 아래: D)\n',
 	COMMAND_INPUT: '\n게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)\n',
