3944b7f1a1d2dedf3540c429e2314e73c8639823
Minhye Kang <alsgp9898@naver.com>
2022-12-13 19:43:38 +0900

    test: 입력 유효성 검사 테스트 코드 작성

--- __tests__/inputTest.js
+++ __tests__/inputTest.js
@@ -1,4 +1,4 @@
-const { checkBridgeSize } = require("../src/Validation");
+const { checkBridgeSize, checkMovingSpace, checkGameCommand } = require("../src/Validation");
 
 describe("다리 길이 입력 유효성 검사", () => {
   test.each([["a"], ["#"], [" "]])(
@@ -22,6 +22,70 @@ describe("다리 길이 입력 유효성 검사", () => {
     (input) => {
       expect(() => {
         checkBridgeSize(input);
+      }).not.toThrow();
+    }
+  );
+});
+
+describe('이동할 칸 입력 유효성 테스트', () => {
+  test.each([['u'], ['d']])('이동할 칸이 소문자 u나 d로 입력되면 예외가 발생한다.', (input) => {
+    expect(() => {
+      checkMovingSpace(input);
+    }).toThrow('[ERROR] 대문자로 입력해주세요.');
+  });
+  test.each([['A'], ['1'], ['@'], [''], [' ']])(
+    '이동할 칸이 U나 D가 아니면 예외가 발생한다.',
+    (input) => {
+      expect(() => {
+        checkMovingSpace(input);
+      }).toThrow('[ERROR] 이동할 칸은 U나 D만 입력할 수 있습니다.');
+    }
+  );
+  test.each([['UD'], ['UUU'], ['ABC']])(
+    '이동할 칸이 2자리 이상의 문자면 예외가 발생한다.',
+    (input) => {
+      expect(() => {
+        checkMovingSpace(input);
+      }).toThrow('[ERROR] 이동할 칸은 U나 D만 입력할 수 있습니다.');
+    }
+  );
+  test.each([['U'], ['D']])(
+    '이동할 칸이 U나 D가 들어오면 아무 문제도 발생하지 않는다..',
+    (input) => {
+      expect(() => {
+        checkMovingSpace(input);
+      }).not.toThrow();
+    }
+  );
+});
+
+describe('재시작 여부 입력 유효성 테스트', () => {
+  test.each([['r'], ['q']])('소문자 r이나 q가 입력되면 예외가 발생한다.', (input) => {
+    expect(() => {
+      checkGameCommand(input);
+    }).toThrow('[ERROR] 대문자로 입력해주세요.')
+  });
+  test.each([['U'], ['1'], ['@'], [''], [' ']])(
+    '입력된 명령어가 R이나 Q가 아니면 예외가 발생한다.',
+    (input) => {
+      expect(() => {
+        checkGameCommand(input);
+      }).toThrow('[ERROR] R이나 Q만 입력할 수 있습니다.');
+    }
+  );
+  test.each([['RQ'], ['RRR'], ['ABC']])(
+    '입력된 명령어가 2자리 이상의 문자면 예외가 발생한다.',
+    (input) => {
+      expect(() => {
+        checkGameCommand(input);
+      }).toThrow('[ERROR] R이나 Q만 입력할 수 있습니다.');
+    }
+  );
+  test.each([['R'], ['Q']])(
+    '이동할 칸이 R이나 Q가 들어오면 아무 문제도 발생하지 않는다.',
+    (input) => {
+      expect(() => {
+        checkGameCommand(input);
       }).not.toThrow();
     }
   );
